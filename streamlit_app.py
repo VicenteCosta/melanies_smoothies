bcbe441e4b4d7b882df19f7e545502cdea5974f0
Vicente Costa <81699135+VicenteCosta@users.noreply.github.com>
2025-10-31 17:59:56 +0100

    Add requests import and fetch watermelon data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -30,6 +31,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
 
     #st.write(ingredients_list)
 
@@ -45,7 +48,5 @@ if ingredients_list:
         st.success('Your '+name_on_order+' Smoothie is ordered!', icon="✅") 
 
 # New Section
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
+
