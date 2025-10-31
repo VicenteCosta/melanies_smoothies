ba2e6435566f25f077972d9d561181fbc8900eb6
Vicente Costa <81699135+VicenteCosta@users.noreply.github.com>
2025-10-31 17:57:40 +0100

    Fix typo in variable name for smoothie data

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothifroot_response.json(),user_container_width=True)
+st_df = st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
